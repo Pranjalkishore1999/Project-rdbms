590c68c824030e102751dbe2c1487ee91729918d
Pranjalkishore1999 <43421513+Pranjalkishore1999@users.noreply.github.com>
2021-02-08 23:35:56 +0530

    Delete index.js

--- Telegram bot/index.js
+++
@@ -1,5 +0,0 @@
-//const TelegramBot = require("node-telegram-bot-api")
-var TelegramBot= require('node-telegram-bot-api');
-var token ='1627536932:AAG8xwoZKApq79u5C6WTIKgjD_370D06tfg';
-var bot =new TelegramBot(token,{polling:true});
-bot.onText(/\/echo (.+)/,function(msg,match))
